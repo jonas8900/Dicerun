507a0837e7212d8e1a44c14ebd930e6ae781b891
Jonas Dally <jonas.dally@hotmail.de>
2025-03-21 21:20:11 +0100

    change logo file

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,4 +1,3 @@
-
 import TextButton from "@/components/Buttons/TextSubmitButton";
 import Icon from "@/components/Icons/iconComponent";
 import CredentialInput from "@/components/Inputs/credentialInput";
@@ -14,118 +13,123 @@ import ToastSuccess from "@/components/ToastMessages/Success";
 import ToastDanger from "@/components/ToastMessages/Danger";
 
 export default function Login({ clicks, setClicks, className }) {
-    const router = useRouter();
-    const [typeSwitch, setTypeSwitch] = useState('password');
-    const [toastMessage, setToastMessage] = useState('');
-    const [error, setError] = useState(null);
+  const router = useRouter();
+  const [typeSwitch, setTypeSwitch] = useState("password");
+  const [toastMessage, setToastMessage] = useState("");
+  const [error, setError] = useState(null);
 
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-    async function handleSubmit(event) {
-        event.preventDefault();
+    const formData = new FormData(event.target);
+    const email = formData.get("email");
+    const password = formData.get("password");
 
-        const formData = new FormData(event.target);
-        const email = formData.get("email");
-        const password = formData.get("password");
+    setError(null);
 
-        setError(null);
-    
-        const response = await signIn("credentials", {
-          redirect: false,
-          email,
-          password,
-        });
-    
-        if (response.error) {
-          setError(response.error);
-          setToastMessage('Email oder Passwort stimmen nicht überein');
-          setTimeout(() => {
-            setToastMessage(''); 
-            router.push('/auth/login');
-          }, 4000);
-        } else {
-          setToastMessage('Erfolgreich eingeloggt! 🎉')
-          setTimeout(() => {
-            setToastMessage(''); 
-            router.push('/'); 
-          }, 4000);
-        }
+    const response = await signIn("credentials", {
+      redirect: false,
+      email,
+      password,
+    });
 
+    if (response.error) {
+      setError(response.error);
+      setToastMessage("Email oder Passwort stimmen nicht überein");
+      setTimeout(() => {
+        setToastMessage("");
+        router.push("/auth/login");
+      }, 4000);
+    } else {
+      setToastMessage("Erfolgreich eingeloggt! 🎉");
+      setTimeout(() => {
+        setToastMessage("");
+        router.push("/");
+      }, 4000);
     }
-  
-    function handlePasswortTypeHidden() {
-      setTypeSwitch('text');
   }
-  
+
+  function handlePasswortTypeHidden() {
+    setTypeSwitch("text");
+  }
+
   function handlePasswortTypeVisibil() {
-      setTypeSwitch('password');
+    setTypeSwitch("password");
   }
-  
 
-
-    return(
-        <>
-              <>
-              <ToastSuccess message={toastMessage} onClose={() => setToastMessage('')} />
-              <ToastDanger message={error} onClose={() => setToastMessage(null)} />
-                <StyledImage src={"/images/Logo.png"} width={200} height={200} alt="Logo" />
-                  <StyledForm onSubmit={handleSubmit} className={className}>
-                    <StyledInputContainer>
-                      <StyledLabel htmlFor="email" />
-                      <CredentialInput
-                        type="email"
-                        id="email"
-                        name="email"
-                        placeholder="Email"
-                        required
-                      />
-                      <StyledLabel htmlFor="password" />
-                      <VisibilityIcon onTouchEnd={handlePasswortTypeVisibil} onTouchStart={handlePasswortTypeHidden}>
-                      <CredentialInput
-                        type={typeSwitch}
-                        id="password"
-                        name="password"
-                        placeholder="Passwort"
-                        required
-                      />
-                        <StyledIcon IconComponent={typeSwitch === "password" ? IoMdEye : IoEyeOff}/>
-                      </VisibilityIcon>
-                      <TextButton text="Login" type="submit"/>
-                    </StyledInputContainer>
-                    <StyledOrContainer>
-                      <StyledDivLine></StyledDivLine>
-                      <StyledParagraph>oder</StyledParagraph>
-                      <StyledDivLine></StyledDivLine>
-                    </StyledOrContainer>
-                    {/* <StyledLoginMethodsContainer>
+  return (
+    <>
+      <>
+        <ToastSuccess
+          message={toastMessage}
+          onClose={() => setToastMessage("")}
+        />
+        <ToastDanger message={error} onClose={() => setToastMessage(null)} />
+        <StyledImage
+          src={"/images/Logo.png"}
+          width={200}
+          height={200}
+          alt="Logo"
+        />
+        <StyledForm onSubmit={handleSubmit} className={className}>
+          <StyledInputContainer>
+            <StyledLabel htmlFor="email" />
+            <CredentialInput
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              required
+            />
+            <StyledLabel htmlFor="password" />
+            <VisibilityIcon
+              onTouchEnd={handlePasswortTypeVisibil}
+              onTouchStart={handlePasswortTypeHidden}>
+              <CredentialInput
+                type={typeSwitch}
+                id="password"
+                name="password"
+                placeholder="Passwort"
+                required
+              />
+              <StyledIcon
+                IconComponent={typeSwitch === "password" ? IoMdEye : IoEyeOff}
+              />
+            </VisibilityIcon>
+            <TextButton text="Login" type="submit" />
+          </StyledInputContainer>
+          <StyledOrContainer>
+            <StyledDivLine></StyledDivLine>
+            <StyledParagraph>oder</StyledParagraph>
+            <StyledDivLine></StyledDivLine>
+          </StyledOrContainer>
+          {/* <StyledLoginMethodsContainer>
                       <LoginWithButton IconComponent={FcGoogle} />
                       <LoginWithButton IconComponent={FaApple} />
                     </StyledLoginMethodsContainer> */}
-                    <StyledParagraphLink>
-                      noch keinen Account?{" "}
-                      <Link href="/registration">Registriere dich jetzt</Link>
-                    </StyledParagraphLink>
-                </StyledForm>
-              </>
-       </>
-    )
+          <StyledParagraphLink>
+            noch keinen Account?{" "}
+            <Link href="/registration">Registriere dich jetzt</Link>
+          </StyledParagraphLink>
+        </StyledForm>
+      </>
+    </>
+  );
 }
 
-
 const StyledImage = styled(Image)`
-  width: 100%; 
-  height: 100%; 
-  
+  width: 100%;
+  height: 100%;
 `;
 
 const StyledForm = styled.form`
-  width: 80%;  
+  width: 80%;
   margin: auto;
 `;
 
 const StyledInputContainer = styled.div`
   display: flex;
   flex-direction: column;
-  
 `;
 
 const StyledLabel = styled.label`
@@ -150,8 +154,6 @@ const StyledParagraph = styled.p`
   color: var(--subtitle-color);
 `;
 
-
-
 const StyledParagraphLink = styled.p`
   font-size: 0.875rem;
   color: var(--brand-primary);
@@ -164,7 +166,6 @@ const VisibilityIcon = styled.div`
   position: relative;
   padding: 0;
   margin: 0;
-  
 `;
 
 const StyledIcon = styled(Icon)`
@@ -176,6 +177,5 @@ const StyledIcon = styled(Icon)`
   top: 50%;
   transform: translateY(-55%);
   cursor: pointer;
-  color: #CECECE;
+  color: #cecece;
 `;
-
